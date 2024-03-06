26f6fffa5650b7e2ad5fbe1c5c83c208fb837230
greglabo78 <gregoryobigodwin@yahoo.com>
2024-03-06 14:02:29 +0000

    generic post view

--- blog/views.py
+++ blog/views.py
@@ -6,6 +6,8 @@ from .models import Post
 
 # Create your views here.
 class PostList(generic.ListView):
-    model = Post
+    queryset = Post.objects.all()
+    template_name = "post_list.html"
     
+
 
